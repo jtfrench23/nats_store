54d3fde2680c482766143d34b96958761e0ecc93
jtfrench23 <92399356+jtfrench23@users.noreply.github.com>
2022-03-19 18:39:14 -0500

    added validation of password on owner creation

--- store/views.py
+++ store/views.py
@@ -5,6 +5,7 @@ from .forms import ProductForm, EditForm
 from django.http import HttpResponseRedirect
 import bcrypt
 from cart.forms import CartAddProductForm
+from django.contrib import messages
 
 def index(request):
     context={
@@ -19,17 +20,21 @@ def owner_login(request):
 
 def register_owner(request):    
     # include some logic to validate user input before adding them to the database!
-    password = request.POST['password']
-    pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()  # create the hash    
-    print(pw_hash,"$$$$$$$$$$$$$$$$$$$$")      # prints something like b'$2b$12$sqjyok5RQccl9S6eFLhEPuaRaJCcH3Esl2RWLm/cimMIEnhnLb7iC'    
-    # be sure you set up your database so it can store password hashes this long (60 characters)
-    # make sure you put the hashed password in the database, not the one from the form!
-    Owner.objects.create(email=request.POST['email'], password=pw_hash) 
-    user = Owner.objects.filter(email=request.POST['email'])
-    if user: # note that we take advantage of truthiness here: an empty list will return false
-        logged_user = user[0] 
-        request.session['owner_id'] = logged_user.id
-    return redirect("/product_manager") # never render on a post, always redirect!  
+    if request.POST['password']==request.POST['password_confirm']:
+        password = request.POST['password']
+        pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()  # create the hash    
+        print(pw_hash,"$$$$$$$$$$$$$$$$$$$$")      # prints something like b'$2b$12$sqjyok5RQccl9S6eFLhEPuaRaJCcH3Esl2RWLm/cimMIEnhnLb7iC'    
+        # be sure you set up your database so it can store password hashes this long (60 characters)
+        # make sure you put the hashed password in the database, not the one from the form!
+        Owner.objects.create(email=request.POST['email'], password=pw_hash) 
+        user = Owner.objects.filter(email=request.POST['email'])
+        if user: # note that we take advantage of truthiness here: an empty list will return false
+            logged_user = user[0] 
+            request.session['owner_id'] = logged_user.id
+        return redirect("/product_manager")
+    else:
+        messages.error(request, 'Passwords do not match')
+        return redirect('/owner') # never render on a post, always redirect!  
 
 
 def validate_owner_login(request):
