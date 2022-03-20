39a2b20bb983f144799c6ddaea42e547da91403a
jtfrench23 <92399356+jtfrench23@users.noreply.github.com>
2022-03-19 22:27:24 -0500

    added orders page

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 
 
@@ -34,7 +35,8 @@ class Order (models.Model):
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_CHOICES, default='P')
-    customer= models.ForeignKey(Customer, on_delete=models.PROTECT)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    shipped = models.BooleanField(default=False)
 class Order_Item(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
@@ -42,7 +44,7 @@ class Order_Item(models.Model):
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
-class address(models.Model):
+class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
