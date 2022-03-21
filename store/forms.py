5654325406d0c6841e7301bf77ebcf46396d8558
jtfrench23 <92399356+jtfrench23@users.noreply.github.com>
2022-03-20 21:52:40 -0500

    added bootstrap to add product form

--- store/forms.py
+++ store/forms.py
@@ -5,13 +5,13 @@ from .models import Product
 
 #Create product form
 class ProductForm(forms.Form):
-    name = forms.CharField()
-    slug = forms.SlugField()
-    description = forms.CharField(widget=forms.Textarea)
-    product_type= forms.CharField()
-    price = forms.DecimalField()
-    size = forms.CharField()
-    stock = forms.IntegerField()
+    name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Name', 'style': 'width: 300px;', 'class': 'form-control'}))
+    slug = forms.SlugField(widget=forms.TextInput(attrs={'placeholder': 'slug', 'style': 'width: 300px;', 'class': 'form-control'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control', 'style': 'width: 300px;'}))
+    product_type= forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'type', 'style': 'width: 300px;', 'class': 'form-control'}))
+    price = forms.DecimalField(widget=forms.NumberInput(attrs={'placeholder': '1.99', 'style': 'width: 300px;', 'class': 'form-control'}))
+    size = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'size', 'style': 'width: 300px;', 'class': 'form-control'}))
+    stock = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': '1', 'style': 'width: 300px;', 'class': 'form-control'}))
     product_image = forms.ImageField()
 class EditForm(forms.Form):
     name = forms.CharField()
